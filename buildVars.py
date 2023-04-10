5f7197ef395c7cfd55f81b002ac05fd77ad97cb8
David Parduhn <xkill85@gmx.net>
2023-04-10 18:50:10 +0200

    updated to nvda 2023.1

--- buildVars.py
+++ buildVars.py
@@ -20,12 +20,12 @@ addon_info = {
 	"addon-description" : _("""This add-on uses an own-built editor to create an appmodule for the currently running aplication.
 """),
 	# version
-	"addon-version" : "0.2",
+	"addon-version" : "0.2.1",
 	# Author(s)
 	"addon-author" : "David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon-url" : None,
-	"lastTestedNVDAVersion": "2022.1"
+	"lastTestedNVDAVersion": "2023.1"
 }
 
 
